5bdf7b33aa4d969d15e5996fd6f21817003feedf
shgysd <shgysd@gmail.com>
2019-03-02 21:05:33 +0900

    Set up redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
-export default () => (
-  <View style={{ marginTop: 50 }} testID="hello">
-    <Text>Hello Expo!</Text>
-  </View>
-);
+import configureStore from './store/configureStore';
+
+const App = () => {
+  return (
+    <Provider store={configureStore}>
+      <View style={{ marginTop: 50 }} testID="hello">
+        <Text>Hello Expo!</Text>
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/reducers/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import routine from './routine';
+
+export default combineReducers({
+  routine,
+});
