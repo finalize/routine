431bb8376e9537987c879643fdff9ac744d1b49d
shgysd <shgysd@gmail.com>
2019-03-02 21:54:32 +0900

    Add types

--- src/actions/routine.ts
+++ src/actions/routine.ts
@@ -2,7 +2,7 @@ import {
   TOGGLE_MODAL_ADD_ROUTINE,
 } from '../consistants/actionTypes';
 
-export const toggleModalAddRoutine = (visible: boolean) => ({
+export const toggleModalAddRoutine = (visible: boolean): { visible: boolean, type: string } => ({
   visible,
   type: TOGGLE_MODAL_ADD_ROUTINE,
 });
